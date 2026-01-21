95b507994ce2bc3abf17382d08b7201579f9dc97
e7he <e7he@uwaterloo.ca>
2026-01-21 00:50:10 -0500

    Add slab allocator for the task descriptors and task stack frames.

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ extern char* rodata;
 
 void bar() {
   while (true) {
-    Uart::syncPrint(Uart::kConsole, "yes");
+    Uart::syncPrint(Uart::CONSOLE, "yes");
   }
 }
 
@@ -29,8 +29,8 @@ int kmain() {
   }
 
   // Set up UART.
-  Uart::configAndEnable(Uart::kConsole);
-  Uart::syncPrint(Uart::kConsole, "Kitty kernel version: " __DATE__ " / " __TIME__ "");
+  Uart::configAndEnable(Uart::CONSOLE);
+  Uart::syncPrint(Uart::CONSOLE, "Kitty kernel version: " __DATE__ " / " __TIME__ "");
 
   // Schedule a bar task.
   int tid = Create(Priority::MEDIUM, bar);
@@ -38,7 +38,7 @@ int kmain() {
 
   TaskDescriptor& taskp = TaskScheduler::scheduleNextTask();
   kit::formatString(buffer, "Task ID %d, address %X %X", tid, &tid, &taskp);
-  Uart::syncPrint(Uart::kConsole, buffer);
+  Uart::syncPrint(Uart::CONSOLE, buffer);
 
   for (;;) {
     TaskDescriptor& task = TaskScheduler::scheduleNextTask();
