ef43a1ef5907a7c0e3b582bcf8c1d7fe8f35141a
e7he <e7he@uwaterloo.ca>
2026-01-24 21:09:01 -0500

    Remove unused function.

--- kit_algorithm.h
+++ kit_algorithm.h
@@ -10,12 +10,3 @@ extern "C" {
 void* memset(void* s, int c, size_t n);
 void* memcpy(void* dest, const void* src, size_t n);
 }
-
-namespace kit {
-template <typename It, typename T> It find(It begin, It end, const T& value) {
-  while (begin != end && *begin != value) {
-    ++begin;
-  }
-  return begin;
-}
-} // namespace kit
