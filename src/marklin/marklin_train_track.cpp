69815a7c548cedf1aa316798966a2e01a7faa7ba
Ian Zhao <i6zhao@uwaterloo.ca>
2026-04-05 01:24:37 -0400

    smoothly deaccel when offlinespeed is 0

--- src/marklin/marklin_train_track.cpp
+++ src/marklin/marklin_train_track.cpp
@@ -2408,10 +2408,10 @@ TrackNode* TrainTrackState::getTrackNodeByName(const char* name) {
 
 void KinematicsSystem::onTick(TrainTrackState& ttState, TrainId trainId) {
   // Apply the acceleration to update the estimated speed.
+  static constexpr Speed ACCEL_UM_PER_TICK_PER_TICK = 10;
+  static constexpr Speed DEACCEL_UM_PER_TICK_PER_TICK = -15;
   if (Speed targetSpeed = marklin::convertSpeedLevelToOfflineSpeed(trainId, offlineSpeedLevel);
       offlineSpeed != targetSpeed) {
-    static constexpr Speed ACCEL_UM_PER_TICK_PER_TICK = 10;
-    static constexpr Speed DEACCEL_UM_PER_TICK_PER_TICK = -15;
     Speed speedDiff = targetSpeed - offlineSpeed;
     Speed delta = kit::clamp(speedDiff, DEACCEL_UM_PER_TICK_PER_TICK, ACCEL_UM_PER_TICK_PER_TICK);
     if (offlineSpeed == 0) {
@@ -2422,7 +2422,7 @@ void KinematicsSystem::onTick(TrainTrackState& ttState, TrainId trainId) {
     offlineSpeed += delta;
     estimatedSpeed = kit::max(0, estimatedSpeed);
   } else if (offlineSpeed == 0) {
-    estimatedSpeed = 0;
+    estimatedSpeed = kit::max(0, estimatedSpeed + DEACCEL_UM_PER_TICK_PER_TICK);
   }
 
   // Apply the estimated speed to update the estimated position.
